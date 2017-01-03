8fb73f66ef5178310d84cf8d5047edb3db00cc9c
Yannick Van Oekelen <r0298651@student.thomasmore.be>
2017-01-03 19:17:37 +0100

    New changelist

--- Validation.js
+++ Validation.js
@@ -1,3 +1,21 @@
 /**
  * Created by yannickvanoekelen on 3/01/17.
  */
+module.exports = {
+
+    fieldsNotEmpty: function (object) {
+        var errors = [];
+        //console.log(object);
+        for (i = 1; i < arguments.length; i++) {
+            if (!this.fieldNotEmpty(object, arguments[i])) {
+                errors.push(arguments[i]);
+            }
+        };
+        return errors.length === 0 ? null : errors;
+    },
+
+    fieldNotEmpty: function (object, field) {
+        return object && object[field] && object[field] !== "";
+    }
+
+};
